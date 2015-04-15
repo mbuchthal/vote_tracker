ba2c0f8b1040a52445cb86e53b5bc8bad0996738
Mark Buchthal <mark.buchthal@gmail.com>
2015-04-15 14:14:47 -0700

    add imgur image pull

--- vote_tracker.js
+++ vote_tracker.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
 
 var Photo = function (location, description) {
@@ -9,21 +8,23 @@ var Photo = function (location, description) {
   this.winPercent;
 };
 var kittenArr = [];
-kittenArr.push(new Photo('Images/kitten1.jpg', 'Holding a ribbon.'));
-kittenArr.push(new Photo('Images/kitten2.jpg', 'Tiny furball in a hand.'));
-kittenArr.push(new Photo('Images/kitten3.jpg', 'Too small too climb.'));
-kittenArr.push(new Photo('Images/kitten4.jpg', 'Monster-cat!'));
-kittenArr.push(new Photo('Images/kitten5.jpg', 'Finger lickin\' good!'));
-kittenArr.push(new Photo('Images/kitten6.jpg', 'It\'s been a tough week kitties.'));
-kittenArr.push(new Photo('Images/kitten7.jpg', 'I thought it was a litterbox!'));
-kittenArr.push(new Photo('Images/kitten9.jpg', 'I can\t be bothered kitty.'));
-kittenArr.push(new Photo('Images/kitten9.jpg', 'Leave me alone kitty.'));
-kittenArr.push(new Photo('Images/kitten10.jpg', 'Upside down kitty'));
-kittenArr.push(new Photo('Images/kitten11.jpg', 'Eating my own tail kitty'));
-kittenArr.push(new Photo('Images/kitten12.jpg', 'Never let go Rose, kitty.'));
-kittenArr.push(new Photo('Images/kitten13.jpg', 'Take me shopping kitty.'));
-kittenArr.push(new Photo('Images/kitten14.jpg', 'Permanently surprised kitty.'));
 
+// kittenArr.push(new Photo('Images/kitten1.jpg', 'Holding a ribbon.'));
+// kittenArr.push(new Photo('Images/kitten2.jpg', 'Tiny furball in a hand.'));
+// kittenArr.push(new Photo('Images/kitten3.jpg', 'Too small too climb.'));
+// kittenArr.push(new Photo('Images/kitten4.jpg', 'Monster-cat!'));
+// kittenArr.push(new Photo('Images/kitten5.jpg', 'Finger lickin\' good!'));
+// kittenArr.push(new Photo('Images/kitten6.jpg', 'It\'s been a tough week kitties.'));
+// kittenArr.push(new Photo('Images/kitten7.jpg', 'I thought it was a litterbox!'));
+// kittenArr.push(new Photo('Images/kitten9.jpg', 'I can\t be bothered kitty.'));
+// kittenArr.push(new Photo('Images/kitten9.jpg', 'Leave me alone kitty.'));
+// kittenArr.push(new Photo('Images/kitten10.jpg', 'Upside down kitty'));
+// kittenArr.push(new Photo('Images/kitten11.jpg', 'Eating my own tail kitty'));
+// kittenArr.push(new Photo('Images/kitten12.jpg', 'Never let go Rose, kitty.'));
+// kittenArr.push(new Photo('Images/kitten13.jpg', 'Take me shopping kitty.'));
+// kittenArr.push(new Photo('Images/kitten14.jpg', 'Permanently surprised kitty.'));
+
+// var images;
 
 function getRandomKitty() {  //generate 2 random numbers, if equal, then generate another two
   var num = Math.floor(Math.random() * kittenArr.length);
@@ -33,6 +34,8 @@ function getRandomKitty() {  //generate 2 random numbers, if equal, then generat
   while (num == num2) {
       var num2 = Math.floor(Math.random() * kittenArr.length);
     }
+
+
   $('#kittypic1').append($('<img src="'+ kittenArr[num].location +'"></img>')); //renders the pictures
   $('#kittypic2').append($('<img src="'+ kittenArr[num2].location +'"></img>'));
   $('#figcap1').append(kittenArr[num].description);  //renders the captions
@@ -40,6 +43,7 @@ function getRandomKitty() {  //generate 2 random numbers, if equal, then generat
   $('figure').on('mouseover', function() {  //highlight the hovered pic
     $('this').addClass('highlight');
   });
+
 
 function Tracker () {
   $('#kittypic1').on('click'), function() {
@@ -53,50 +57,51 @@ function Tracker () {
   }
 };
 
-getRandomKitty();
-
-var ctx = $("#myChart").get(0).getContext("2d");
-var myDoughnutChart = new Chart(ctx[1]).Doughnut(data,options);
-var data = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
+$.ajax({
+    url: 'https://api.imgur.com/3/album/yNZqw.json',
+    method: 'GET',
+    headers: {
+      'Authorization': 'Client-ID 05ed6615f30f853'
     }
-]
-
-// function Tracker() {
-//   getRandomKitty();
-
-
-// Tracker();
-
-//   this.wins = wins;
-//   this.losses = losses;
-//   this.tallyResults = function() { //function to show how many wins and %
-//     var voteOne;
-//     var voteTwo;
-//   }
-// }
-
-
-
-
-
+  })
+    .done(function(res) {
+      $(res.data.images).each(function(i) {
+        var imgurList = res.data.images;
+        kittenArr.push(new Photo(imgurList[i].link, imgurList[i].title));
+      })
+       getRandomKitty();
+    })
+    .fail(function(err) {
+      console.log(err);
+  });
 });
+
+// var ctx = $("#myChart").get(0).getContext("2d");
+// var myDoughnutChart = new Chart(ctx[1]).Doughnut(data,options);
+// var data = [
+//     {
+//         value: 300,
+//         color:"#F7464A",
+//         highlight: "#FF5A5E",
+//         label: "Red"
+//     },
+//     {
+//         value: 50,
+//         color: "#46BFBD",
+//         highlight: "#5AD3D1",
+//         label: "Green"
+//     },
+//     {
+//         value: 100,
+//         color: "#FDB45C",
+//         highlight: "#FFC870",
+//         label: "Yellow"
+//     }
+// ]
+
+
+
+
 
 
 
