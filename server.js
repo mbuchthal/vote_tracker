c5b033408f2c89918a9407c22fd8aa7f75e9090b
Mark Buchthal <mark.buchthal@gmail.com>
2015-04-16 14:56:23 -0700

    add nodejs and express with server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+
+var express = require('express');
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.get('/greet/:person', function(request, response) {
+  response.status(200).send('<h1>Hello Stranger!</h1>');
+});
+
+app.get('/secret', function(request, response) {
+  response.status(200).send('<h1>In Sixth Sense, Bruce Willis is already dead.</h1>');
+});
+
+app.get('/*', function(request, response) {
+  response.status(404).send('Could Not Find Page - Error 404');
+});
+
+app.listen(process.env.PORT || 5000, function() {
+  console.log('server running');
+});
